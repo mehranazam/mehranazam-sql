5fe3a556bbb31898cc50e14174404d66ab9d1538
Mehran Azam <moinulazam@MehranAcBookPro.home>
2022-03-18 15:15:37 -0400

    m06

---
+++ rcttc-schema.sql
@@ -0,0 +1,55 @@
+drop database if exists rcttc;
+create database rcttc;
+
+create table customers(
+
+customer_id int primary key auto_increment,
+
+customer_first char(50) not null,
+
+customer_last char(50) not null,
+
+customer_email char(50) not null,
+
+
+
+
+
+);
+
+
+create table theaters(
+
+
+
+
+
+
+
+);
+
+
+
+
+create table shows(
+
+
+
+
+
+
+
+
+);
+
+
+
+create table tickets(
+
+
+
+
+
+
+
+);
