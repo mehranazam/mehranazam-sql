53a4b278a33db8539a598ae87a7e926479f0d54e
Mehran Azam <moinulazam@MehranAcBookPro.home>
2022-03-21 12:34:22 -0400

    m06

--- rcttc-schema.sql
+++ rcttc-schema.sql
@@ -26,9 +26,7 @@
 -- 			is there a direct relationship at all? 					no
 
 -- 2, 4
--- 			is there a direct relationship at all? 					yes
--- 			can a ticket be associated with multiple theaters?		no
--- 			can a theater be associated with multiple tickets?		yes
+-- 			is there a direct relationship at all? 					no
 
 -- 2, 5
 -- 			is there a direct relationship at all? 					yes
@@ -52,6 +50,10 @@
 drop database if exists rcttc;
 create database rcttc;
 
+
+use rcttc;
+
+
 create table customers(
 
 customer_id int primary key auto_increment,
@@ -65,8 +67,6 @@ customer_email char(50) not null,
 customer_phone char(50),
 
 customer_address char(50)
-
-
 
 );
 
@@ -83,10 +83,6 @@ theater_address char(100) not null,
 
 theater_email char(100) not null
 
-
-
-
-
 );
 
 
@@ -99,7 +95,6 @@ play_id int primary key auto_increment,
 
 play_name char(50) not null
 
-
 );
 
 
@@ -107,12 +102,19 @@ create table showings(
 
 show_id int primary key auto_increment,
 
-show_date date not null
+show_date date not null,
 
+theater_id int not null,
 
+play_id int not null,
 
+constraint fk_showing_theater_id
+foreign key(theater_id)
+references theaters(theater_id),
 
-
+constraint fk_showing_play_id
+foreign key(play_id)
+references plays(play_id)
 );
 
 
@@ -123,16 +125,32 @@ ticket_id int primary key auto_increment,
 
 ticket_price decimal (4, 2) not null,
 
-seat_num char(2) not null
+seat_num char(2) not null,
 
+customer_id int not null,
 
+-- theater_id int not null,
 
+show_id int not null,
 
+constraint fk_ticket_customer_id
+foreign key(customer_id)
+references customers(customer_id),
+
+/*
+constraint fk_ticket_theater_id
+foreign key(theater_id)
+references theaters(theater_id),
+*/
+
+constraint fk_ticket_show_id
+foreign key(show_id)
+references showings(show_id)
 
 );
 
 
-
+/*
 create table customers_tickets(
 
 customer_id int,
@@ -207,3 +225,43 @@ constraint fk_ticket_ticket_showing foreign key(ticket_id) references tickets(ti
 constraint fk_showing_ticket_showing foreign key(show_id) references showings(show_id)
 
 );
+*/
+
+
+select distinct customers
+from `rcttc-data`;
+
+insert into customers(customer_first, customer_last, customer_email, customer_phone, customer_address)
+select distinct customers
+from `rcttc-data`;
+
+select * from customers;
+
+
+
+/*
+insert into theaters(theater_name, theater_phone, theater_address, theater_email)
+select theater_name, theater_phone, theater_address, theater_email
+from `rcttc-data`;
+
+insert into plays(play_name)
+select `show`
+from `rcttc-data`;
+
+insert into tickets(ticket_price, seat_num)
+select ticket_price, seat
+from `rcttc-data`;
+
+insert into showings(show_date)
+select date
+from `rcttc-data`;
+*/
+
+/*
+select *
+from customers c
+inner join tickets ti on ti.customer_id = c.customer_id
+inner join showings sh on sh.show_id = ti.show_id
+inner join theaters th on th.theater_id = sh.theater_id
+inner join plays p on p.play_id = sh.play_id;
+*/
