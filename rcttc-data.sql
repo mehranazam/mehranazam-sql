ddbab0e808e99c0eca9468f2f6b8161e3787638b
Mehran Azam <moinulazam@MehranAcBookPro.home>
2022-03-22 22:59:09 -0400

    m06

---
+++ rcttc-data.sql
@@ -0,0 +1,128 @@
+use rcttc;
+
+
+insert into customers(customer_first, customer_last, customer_email, customer_phone, customer_address)
+select distinct 
+customer_first, 
+customer_last, 
+customer_email, 
+customer_phone, 
+customer_address
+from `rcttc-data`;
+
+
+
+select * from customers;
+
+
+insert into theaters(theater_name, theater_address, theater_email, theater_phone)
+select distinct
+theater,
+theater_address,
+theater_email,
+theater_phone
+from `rcttc-data`;
+
+select * from theaters;
+
+
+insert into plays(play_name)
+select distinct `show`
+from `rcttc-data`;
+
+select * from plays;
+
+
+insert into showings(theater_id, play_id, show_date)
+select distinct
+th.theater_id,
+p.play_id,
+rd.`date`
+from `rcttc-data` rd
+inner join theaters th on rd.theater = th.theater_name
+inner join plays p on rd.`show` = p.play_name;
+
+select * from showings;
+
+
+
+insert into tickets(customer_id, show_id, ticket_price, seat_num)
+select distinct
+c.customer_id,
+sh.show_id,
+rd.ticket_price,
+rd.seat
+from `rcttc-data` rd
+inner join customers c on rd.customer_first = c.customer_first
+inner join showings sh on rd.`date` = sh.show_date;
+
+select * from tickets;
+
+
+
+select distinct
+sh.show_id,
+c.customer_id,
+th.theater_name,
+p.play_name,
+sh.show_date,
+c.customer_first,
+c.customer_last,
+t.seat_num
+from customers c
+inner join tickets t on t.customer_id = c.customer_id
+inner join showings sh on sh.show_id = t.show_id
+inner join plays p on p.play_id = sh.play_id
+inner join theaters th on th.theater_id = sh.theater_id
+where (c.customer_first = 'Pooh' or c.customer_first = 'Cullen' or
+c.customer_last = 'Bedburrow' or c.customer_last = 'Guirau');
+
+
+
+update tickets set
+ticket_price = 22.25
+where show_id = 5;
+
+update tickets set
+seat_num = 'B4'
+where show_id = 5
+and customer_id = 37;
+
+update tickets set
+seat_num = 'C2'
+where show_id = 5
+and customer_id = 38;
+
+
+update customers set
+customer_phone = '1-801-EAT-CAKE'
+where customer_last = 'Swindles';
+
+select
+c.customer_id
+from customers c
+inner join tickets t on t.customer_id = c.customer_id
+inner join showings sh on sh.show_id = t.show_id
+inner join theaters th on th.theater_id = sh.theater_id
+where th.theater_name = '10 Pin';
+
+delete from tickets
+where customer_id = 7 or
+customer_id = 30 or
+customer_id = 35 or
+customer_id = 39 or
+customer_id = 55 or
+customer_id = 8 or
+customer_id = 10 or
+customer_id = 43 or
+customer_id = 45 or
+customer_id = 46 or
+customer_id = 18 or
+customer_id = 19 or
+customer_id = 25 or
+customer_id = 67 or
+customer_id = 69 or
+customer_id = 44 or
+customer_id = 52 or
+customer_id = 74;
+
--- rcttc-schema.sql
+++ rcttc-schema.sql
@@ -129,19 +129,12 @@ seat_num char(2) not null,
 
 customer_id int not null,
 
--- theater_id int not null,
-
 show_id int not null,
 
 constraint fk_ticket_customer_id
 foreign key(customer_id)
 references customers(customer_id),
 
-/*
-constraint fk_ticket_theater_id
-foreign key(theater_id)
-references theaters(theater_id),
-*/
 
 constraint fk_ticket_show_id
 foreign key(show_id)
@@ -150,118 +143,3 @@ references showings(show_id)
 );
 
 
-/*
-create table customers_tickets(
-
-customer_id int,
-
-ticket_id int,
-
-constraint pk_customer_ticket primary key (customer_id, ticket_id),
-
-constraint fk_customer_customer_ticket foreign key(customer_id) references customers(customer_id),
-
-constraint fk_ticket_customer_ticket foreign key(ticket_id) references tickets(ticket_id)
-
-);
-
-
-create table theaters_tickets(
-
-theater_id int,
-
-ticket_id int,
-
-constraint pk_theater_ticket primary key (theater_id, ticket_id),
-
-constraint fk_theater_theater_ticket foreign key(theater_id) references theaters(theater_id),
-
-constraint fk_ticket_theater_ticket foreign key(ticket_id) references tickets(ticket_id)
-
-);
-
-
-
-create table theaters_showings(
-
-theater_id int,
-
-show_id int,
-
-constraint pk_theater_showing primary key(theater_id, show_id),
-
-constraint fk_theater_theater_showing foreign key(theater_id) references theaters(theater_id),
-
-constraint fk_showing_theater_showing foreign key(show_id) references showings(show_id)
-
-);
-
-
-create table plays_showings(
-
-
-play_id int,
-
-show_id int,
-
-constraint pk_play_showing primary key(play_id, show_id),
-
-constraint fk_play_play_showing foreign key(play_id) references plays(play_id),
-
-constraint fk_showing_play_showing foreign key(show_id) references showings(show_id)
-
-);
-
-create table tickets_showings(
-
-ticket_id int,
-
-show_id int,
-
-constraint pk_ticket_showing primary key(ticket_id, show_id),
-
-constraint fk_ticket_ticket_showing foreign key(ticket_id) references tickets(ticket_id),
-
-constraint fk_showing_ticket_showing foreign key(show_id) references showings(show_id)
-
-);
-*/
-
-
-select distinct customers
-from `rcttc-data`;
-
-insert into customers(customer_first, customer_last, customer_email, customer_phone, customer_address)
-select distinct customers
-from `rcttc-data`;
-
-select * from customers;
-
-
-
-/*
-insert into theaters(theater_name, theater_phone, theater_address, theater_email)
-select theater_name, theater_phone, theater_address, theater_email
-from `rcttc-data`;
-
-insert into plays(play_name)
-select `show`
-from `rcttc-data`;
-
-insert into tickets(ticket_price, seat_num)
-select ticket_price, seat
-from `rcttc-data`;
-
-insert into showings(show_date)
-select date
-from `rcttc-data`;
-*/
-
-/*
-select *
-from customers c
-inner join tickets ti on ti.customer_id = c.customer_id
-inner join showings sh on sh.show_id = ti.show_id
-inner join theaters th on th.theater_id = sh.theater_id
-inner join plays p on p.play_id = sh.play_id;
-*/
