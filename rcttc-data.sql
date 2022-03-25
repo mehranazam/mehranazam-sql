03731c1d089d98f42624153ddb3c02d3e6be9735
Mehran Azam <moinulazam@MehranAcBookPro.home>
2022-03-25 17:30:32 -0400

    m06

--- rcttc-data.sql
+++ rcttc-data.sql
@@ -12,7 +12,7 @@ from `rcttc-data`;
 
 
 
-select * from customers;
+
 
 
 insert into theaters(theater_name, theater_address, theater_email, theater_phone)
@@ -23,14 +23,14 @@ theater_email,
 theater_phone
 from `rcttc-data`;
 
-select * from theaters;
+
 
 
 insert into plays(play_name)
 select distinct `show`
 from `rcttc-data`;
 
-select * from plays;
+
 
 
 insert into showings(theater_id, play_id, show_date)
@@ -42,7 +42,7 @@ from `rcttc-data` rd
 inner join theaters th on rd.theater = th.theater_name
 inner join plays p on rd.`show` = p.play_name;
 
-select * from showings;
+
 
 
 
@@ -53,10 +53,11 @@ sh.show_id,
 rd.ticket_price,
 rd.seat
 from `rcttc-data` rd
-inner join customers c on rd.customer_first = c.customer_first
-inner join showings sh on rd.`date` = sh.show_date;
+inner join customers c on rd.customer_email = c.customer_email
+inner join theaters th on th.theater_name = rd.theater
+inner join showings sh on rd.`date` = sh.show_date and th.theater_id = sh.theater_id;
 
-select * from tickets;
+
 
 
 
@@ -83,15 +84,24 @@ update tickets set
 ticket_price = 22.25
 where show_id = 5;
 
+
+select * 
+from tickets t
+inner join customers c on c.customer_id = t.customer_id
+where show_id = 5;
+
+
 update tickets set
 seat_num = 'B4'
-where show_id = 5
-and customer_id = 37;
+where ticket_id = 98;
 
 update tickets set
 seat_num = 'C2'
-where show_id = 5
-and customer_id = 38;
+where ticket_id = 100;
+
+update tickets set
+seat_num = 'A4'
+where ticket_id = 101;
 
 
 update customers set
@@ -99,40 +109,54 @@ customer_phone = '1-801-EAT-CAKE'
 where customer_last = 'Swindles';
 
 select
-c.customer_id
+c.customer_id,
+c.customer_first,
+c.customer_last
 from customers c
 inner join tickets t on t.customer_id = c.customer_id
 inner join showings sh on sh.show_id = t.show_id
 inner join theaters th on th.theater_id = sh.theater_id
-where th.theater_name = '10 Pin';
+where th.theater_name = '10 Pin'
+order by c.customer_id asc;
+
+
+
+
+
+
+select ticket_id from
+(
+select
+count(*),
+sh.show_id,
+c.customer_id
+from tickets t
+inner join showings sh on t.show_id = sh.show_id
+inner join customers c on c.customer_id = t.customer_id
+inner join theaters th on th.theater_id = sh.theater_id
+where th.theater_name = '10 Pin'
+group by sh.show_id, c.customer_id
+having count(*) = 1
+) as single_res
+inner join tickets t on t.show_id = single_res.show_id 
+and t.customer_id = single_res.customer_id
+;
+
+
 
 delete from tickets
-where customer_id = 7 or
-customer_id = 30 or
-customer_id = 35 or
-customer_id = 39 or
-customer_id = 55 or
-customer_id = 8 or
-customer_id = 10 or
-customer_id = 43 or
-customer_id = 45 or
-customer_id = 46 or
-customer_id = 18 or
-customer_id = 19 or
-customer_id = 25 or
-customer_id = 67 or
-customer_id = 69 or
-customer_id = 44 or
-customer_id = 52 or
-customer_id = 74;
+where ticket_id in (25, 26, 29, 41, 50, 51, 59, 67, 68);
+
+
+
+select *
+from customers
+where customer_last = 'Egle of Germany';
 
 
 
 delete from tickets
-where ticket_id between 532 and 537;
-
-
-
+where customer_id = 65;
 
 
 delete from customers
